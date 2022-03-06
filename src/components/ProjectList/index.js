55ee05127758760da63d4f51fd9194705fe40c8f
Fernando Ortega <feortegas@gmail.com>
2022-03-06 15:33:04 -0600

    add Projects section

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,57 +1,25 @@
 import React from 'react';
-// import redditLite from '../../assets/projects/reddit-lite.png';
 
 function Project(props) {
-    return (
-        <div key={props.project.name}>
-            <a href="https://www.google.com">
-                <h5>
-                    We are awesome
-                </h5>
-                </a>
-
-                <img
-                src={require(`../../assets/projects/${props.project.name}`)}
-                alt={props.project.name}
-                />
-            </div>
-    );
-
-	// <div>
-	// 	<span>My Projects</span>
-	// 	<div className='container'>
-	// 		<div>
-	// 			<div className='col'>Title: Reddit-lite</div>
-	// 			{/* Project number 1 */}
-	// 			<div>
-	// 				<a
-	// 					href='https://afternoon-tundra-62298.herokuapp.com/'
-	// 					target='_blank'
-	// 					rel='noopener noreferrer'>
-	// 					<img
-	// 						src={redditLite}
-	// 						alt='Reddit-lite homepage'
-	// 						style={{ height: 300, width: 300 }}
-	// 					/>
-	// 				</a>
-	// 				<div>
-	// 					<h3>reddit-lite website</h3>
-	// 					<p>
-	// 						Light version of reddit.com. Focus of the application is for the
-	// 						user that is not a die-hard reddit user to be able to focus on
-	// 						the posts and comments only. It also benefits communities where
-	// 						internet connection speed is not as fast as in rich countries.
-	// 					</p>
-	// 				</div>
-	// 			</div>
-	// 		</div>
-	// 		<div className='row align-items-end'>
-	// 			<div className='col'>
-	// 				HTML, CSS, Bootstrap, JavaScript, MySQL, CRUD, Node, Express
-	// 			</div>
-	// 		</div>
-	// 	</div>
-	// </div>
+	const name = props.project.name;
+	const image = props.project.image;
+	const description = props.project.description;
+	const url = props.project.url;
+	return (
+		<div className='col-6'>
+			<a href={url}>
+				<h5>{name}</h5>
+			</a>
+			<img
+				className='project-image'
+				src={require(`../../assets/projects/${image}`)}
+				alt={name}
+				target='_blank'
+				rel='noopener noreferrer'
+			/>
+			<p>{description}</p>
+		</div>
+	);
 }
 
 export default Project;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,46 +4,47 @@ import Project from '../ProjectList';
 function Projects() {
 	const projectArr = [
 		{
-			image: '../../assets/projects/reddit-lite.png',
-			name: 'reddit-lite.png',
+			image: 'reddit-lite.png',
+			name: 'reddit-lite',
 			description:
 				'Light version of reddit.com. Focus of the application is for the user that is not a die-hard reddit user to be able to focus on the posts and comments only. It also benefits communities where internet connection speed is not as fast as in rich countries.',
-			url: 'https://afternoon-tundra-62298.herokuapp.com/'
-		}
-		// {
-		// 	image: '',
-		// 	name: '',
-		// 	description: '',
-		// 	url: '',
-		// },
-		// {
-		// 	image: '',
-		// 	name: '',
-		// 	description: '',
-		// 	url: '',
-		// },
-		// {
-		// 	image: '',
-		// 	name: '',
-		// 	description: '',
-		// 	url: '',
-		// },
-		// {
-		// 	image: '',
-		// 	name: '',
-		// 	description: '',
-		// 	url: '',
-		// },
-		// {
-		// 	image: '',
-		// 	name: '',
-		// 	description: '',
-		// 	url: '',
-		// },
+			url: 'https://afternoon-tundra-62298.herokuapp.com/',
+		},
+		{
+			image: 'spotitube.png',
+			name: 'SpotiTube',
+			description:
+				'SpotiTube web app, allows the user to Search for a Spotify playlist and then insert the tracks to a YouTube playlist (Official videos). Its a colaboration between the Spotify API and YouTube API.',
+			url: 'https://feortegas.github.io/SpotiTube/',
+		},
+		{
+			image: 'run-buddy.jpg',
+			name: 'Run-Buddy',
+			description: 'Run buddy is your portal to connect to your personal trainner and become the journey of getting fit',
+			url: 'https://feortegas.github.io/run-buddy/',
+		},
+		{
+			image: 'city-weather-dashboard.png',
+			name: 'City Weather Dashboard',
+			description: 'Search for the current weather for your local city or future destination. Get also 5 days	Forecast',
+			url: 'https://feortegas.github.io/city-weather-dashboard/',
+		},
+		{
+			image: 'code-quiz.png',
+			name: 'Classic Action Movies Quiz',
+			description: 'Enjoy a short Classic Action Movies Quiz. Think fast or you will run out of time!',
+			url: 'https://feortegas.github.io/code-quiz-classic-movies/',
+		},
+		{
+			image: 'work-day-scheduller.png',
+			name: 'Work day scheduller',
+			description: `Plan your day appointments and to-do's with this Day Scheduller web app`,
+			url: 'https://feortegas.github.io/whats-up-today/',
+		},
 	];
 
 	return (
-		<section>
+		<section className='row d-flex'>
 			{projectArr.map((eachProject) => {
 				return <Project project={eachProject} />;
 			})}
