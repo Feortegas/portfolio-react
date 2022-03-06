63cd9d10662d24056eaa88dc4de89a40f2ab7506
Fernando Ortega <feortegas@gmail.com>
2022-03-06 14:56:49 -0600

    add projectlist and projects components

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,43 +1,53 @@
 import React from 'react';
-import redditLite from '../../assets/projects/reddit-lite.png';
+import Project from '../ProjectList';
 
 function Projects() {
+	const projectArr = [
+		{
+			image: '../../assets/projects/reddit-lite.png',
+			name: 'reddit-lite.png',
+			description:
+				'Light version of reddit.com. Focus of the application is for the user that is not a die-hard reddit user to be able to focus on the posts and comments only. It also benefits communities where internet connection speed is not as fast as in rich countries.',
+			url: 'https://afternoon-tundra-62298.herokuapp.com/'
+		}
+		// {
+		// 	image: '',
+		// 	name: '',
+		// 	description: '',
+		// 	url: '',
+		// },
+		// {
+		// 	image: '',
+		// 	name: '',
+		// 	description: '',
+		// 	url: '',
+		// },
+		// {
+		// 	image: '',
+		// 	name: '',
+		// 	description: '',
+		// 	url: '',
+		// },
+		// {
+		// 	image: '',
+		// 	name: '',
+		// 	description: '',
+		// 	url: '',
+		// },
+		// {
+		// 	image: '',
+		// 	name: '',
+		// 	description: '',
+		// 	url: '',
+		// },
+	];
+
 	return (
-		<div>
-			<span>My Projects</span>
-			<div className='container'>
-				<div>
-					<div className='col'>Title: Reddit-lite</div>
-					{/* Project number 1 */}
-					<div>
-						<a
-							href='https://afternoon-tundra-62298.herokuapp.com/'
-							target='_blank'
-							rel='noopener noreferrer'>
-							<img
-								src={redditLite}
-								alt='Reddit-lite homepage'
-								style={{ height: 300, width: 300 }}
-							/>
-						</a>
-						<div>
-							<h3>reddit-lite website</h3>
-							<p>
-								Light version of reddit.com. Focus of the application is for the
-								user that is not a die-hard reddit user to be able to focus on
-								the posts and comments only. It also benefits communities where
-								internet connection speed is not as fast as in rich countries.
-							</p>
-						</div>
-					</div>
-				</div>
-				<div className='row align-items-end'>
-					<div className='col'>
-						HTML, CSS, Bootstrap, JavaScript, MySQL, CRUD, Node, Express
-					</div>
-				</div>
-			</div>
-		</div>
+		<section>
+			{projectArr.map((eachProject) => {
+				return <Project project={eachProject} />;
+			})}
+		</section>
 	);
 }
 
